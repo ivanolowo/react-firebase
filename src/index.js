dcf57c37ca0cb2baafaad3a6a00b5514f5d4f016
ivan olowo <ivanolowo@ivans-MacBook-Pro.local>
2021-02-20 00:06:42 +0100

    Added firebase

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 
+import Firebase, { FirebaseContext } from './components/Firebase';
+
 ReactDOM.render(
   <React.StrictMode>
     <FirebaseContext.Provider value={new Firebase()}>
       <App />
     </FirebaseContext.Provider>
-  </React.StrictMode >,
+  </React.StrictMode>,
   document.getElementById('root')
 );
